ba265be128e2d18d6f72b4cc9f57f9dde8a048d1
NikitaGusev1 <nkgs370@gmail.com>
2022-07-30 19:18:56 +0300

    navigator changes

--- src/navigation/AppNavigator.tsx
+++ src/navigation/AppNavigator.tsx
@@ -1,15 +1,16 @@
-import { HomeScreen } from '../screens/HomeScreen';
-import { EAppScreens } from '../typescript/statics/EAppScreens';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import React from 'react';
+
+import { HomeScreen } from '../screens/HomeScreen';
+import { EAppScreens } from '../typescript/statics/EAppScreens';
 
 export const AppNavigator = () => {
   const Stack = createNativeStackNavigator();
 
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
         <Stack.Screen name={EAppScreens.Home} component={HomeScreen} />
       </Stack.Navigator>
     </NavigationContainer>
