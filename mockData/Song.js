d0d333326f1d6d26260d44693bb3ad7135f6e91c
NikitaGusev1 <nkgs370@gmail.com>
2022-07-30 19:17:03 +0300

    mock data

---
+++ mockData/Song.js
@@ -0,0 +1,10 @@
+export class Song {
+  constructor(artist, title, minutes, seconds, size, image) {
+    this.artist = artist;
+    this.title = title;
+    this.minutes = minutes;
+    this.seconds = seconds;
+    this.size = size;
+    this.image = image;
+  }
+}
