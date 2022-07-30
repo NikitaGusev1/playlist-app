df1114d89f57d29c3737fedabd3e93e4415eea37
NikitaGusev1 <nkgs370@gmail.com>
2022-07-30 19:18:30 +0300

    theme; button

--- src/theme/defaultTheme.ts
+++ src/theme/defaultTheme.ts
@@ -2,16 +2,33 @@ export type TDefault = typeof defaultTheme;
 
 const defaultTheme = {
   colors: {
-    main: 'cyan',
-    secondary: 'magenta',
-    text: 'black',
-    background: 'white',
+    text: '#000000',
+    background: '#FFFFFF',
+    main: '#FFA500',
   },
+
+  // TODO: windowWidth and windowHeight through Dimensions API
 
   sizes: {
     spacing: 8,
     appPadding: 16,
     header: 24,
+    getSpacing: (multiplier: number) => multiplier * defaultTheme.sizes.spacing,
+  },
+
+  font: {
+    size: {
+      regular: '16px',
+      h1: '24px',
+    },
+    weight: {
+      regular: 400,
+      bold: 700,
+    },
+  },
+
+  roundness: {
+    button: 24,
   },
 };
 
